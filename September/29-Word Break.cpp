ea8bbc7d3dc38ad465b287f0e99ec171e5384158
antdkfa <44724253+antdkfa@users.noreply.github.com>
2020-09-29 23:02:35 +0900

    Update 29-Word Break.cpp

--- September/29-Word Break.cpp
+++ September/29-Word Break.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
+    int visited[1005] = {0,};
     bool ans = false;
-    set<pair<int,int>> s;
     void dfs(string s, vector<string> wordDict, int depth){
-        auto it = s.lowerbound({depth, 0});
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        for(int i = 0;i<s.length();i++){
-            for(int j = 0;j<wordDict.size();j++){
-                if(i + wordDict[j].length() - 1 >= n) continue;
+        if(depth == s.length()) { ans = true; return; }
+        for(int i = 0; i < wordDict.size(); i++){
+            if(depth + wordDict[i].length() - 1 >= s.length() || visited[depth + wordDict[i].length() - 1]) continue;
+            else{
                 int flag = 0;
-                for(int k = 0;k<wordDict[j].length();k++){
-                    if(s[i + k] != wordDict[j][k]) flag = 1;
+                for(int j = 0; j < wordDict[i].length(); j++){
+                    if(s[depth+j] != wordDict[i][j]) {flag = 1; break;}
                 }
-                if(!flag) s.insert({i,i+wordDict[j].length() - 1});
+                if(!flag) visited[depth + wordDict[i].length() - 1] = 1, dfs(s, wordDict,depth+wordDict[i].length());
             }
         }
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        dfs(s,wordDict,0);
         return ans;
     }
 };
