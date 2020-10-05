86ba3016c65e79f72d8073e118b203e045add4fd
antdkfa <44724253+antdkfa@users.noreply.github.com>
2020-10-05 18:55:44 +0900

    Update 5-Complement of Base 10 Integer.cpp

--- October/5-Complement of Base 10 Integer.cpp
+++ October/5-Complement of Base 10 Integer.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int bitwiseComplement(int N) {
+        if(!N) return 1;
+        int ans = 0;
+        stack<int> s;
+        while(N != 0){
+            s.push(N%2);
+            N/=2;
+        }
+        while(!s.empty()){
+            int now = s.top();
+            if(!now) ans += pow(2, s.size() - 1);
+            s.pop();
+        }
+        return ans;
+    }
+};
