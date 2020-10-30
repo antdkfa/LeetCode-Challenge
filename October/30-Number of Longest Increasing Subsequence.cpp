c8f08bc83aaaca4f3ecb971f2fc32dfbb2099051
antdkfa <44724253+antdkfa@users.noreply.github.com>
2020-10-30 18:07:45 +0900

    One Day, One Solve

--- October/30-Number of Longest Increasing Subsequence.cpp
+++ October/30-Number of Longest Increasing Subsequence.cpp
@@ -1,33 +1,49 @@
 class Solution {
-public:
-    int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        
-        // cnt[i][len] = number of increasing sequence of length len ending at i  
-        vector<vector<int>> cnt(n, vector<int>(n+1, 0));
-        for(int i=0; i<n; i++) {
-            cnt[i][1] = 1;
-        }
-        
-        int lis = 1;
-        
-        for(int i=1; i<n; i++) {
-            for(int j=0; j<i; j++) {
-                if(nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                    // add sequences 
-                    cnt[i][dp[i]] += cnt[j][dp[j]];
-                }
-            }
-            lis = max(lis, dp[i]);
-        }
-        
-        // finally count all the subsequence of length lis, ending at any index
-        int ans = 0;
-        for(int i=0; i<n; i++) {
-            ans += cnt[i][lis];
-        }
-        return ans;
+ public:
+  int findNumberOfLIS(vector<int>& nums) {
+    vector<pair<int, int>> mem(nums.size());
+
+    pair<int, int> res{0, 0};
+    for (int i = 0; i < nums.size(); ++i) {
+      auto p = dfs(nums, mem, i);
+
+      if (p.first > res.first) {
+        res.first = p.first;
+        res.second = p.second;
+      } else if (p.first == res.first) {
+        res.second += p.second;
+      }
     }
+
+    return res.second;
+  }
+
+  pair<int, int> dfs(vector<int>& nums,
+                     vector<pair<int, int>>& mem,
+                     int start) {
+    if (mem[start].first > 0) {
+      return mem[start];
+    }
+
+    pair<int, int> res{0, 1};
+
+    for (int i = start + 1; i < nums.size(); ++i) {
+      if (nums[i] > nums[start]) {
+        auto p = dfs(nums, mem, i);
+
+        if (p.first > res.first) {
+          res.first = p.first;
+          res.second = p.second;
+        } else if (p.first == res.first) {
+          res.second += p.second;
+        }
+      }
+    }
+
+    res.first += 1;
+
+    mem[start] = res;
+
+    return res;
+  }
 };
