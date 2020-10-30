5b2ed6fcdaf5db6d3e39be2c4910c077cc0d64eb
antdkfa <44724253+antdkfa@users.noreply.github.com>
2020-10-30 18:06:19 +0900

    One Day, One Solve

--- October/30-Number of Longest Increasing Subsequence.cpp
+++ October/30-Number of Longest Increasing Subsequence.cpp
@@ -1,1 +1,33 @@
-
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        
+        // cnt[i][len] = number of increasing sequence of length len ending at i  
+        vector<vector<int>> cnt(n, vector<int>(n+1, 0));
+        for(int i=0; i<n; i++) {
+            cnt[i][1] = 1;
+        }
+        
+        int lis = 1;
+        
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                    // add sequences 
+                    cnt[i][dp[i]] += cnt[j][dp[j]];
+                }
+            }
+            lis = max(lis, dp[i]);
+        }
+        
+        // finally count all the subsequence of length lis, ending at any index
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            ans += cnt[i][lis];
+        }
+        return ans;
+    }
+};
