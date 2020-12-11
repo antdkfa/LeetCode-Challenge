2f3332e171aa75c546ba5a971cac2fc243e53f03
antdkfa <44724253+antdkfa@users.noreply.github.com>
2020-12-11 17:53:34 +0900

    One Day, One Solve

---
+++ December/11-Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int cnt = 1, idx = 1;
+        for(int i = 1;i<nums.size();i++){
+            if(nums[i] == nums[i-1]){
+                cnt++;
+                if(cnt <= 2) nums[idx++] = nums[i];
+            } 
+            else cnt = 1, nums[idx++] = nums[i];
+        }
+        return idx;
+    }
+};
