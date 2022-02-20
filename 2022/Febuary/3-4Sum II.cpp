61dbbba4fd315b664807328c156d166d29cf6d8b
antdkfa <44724253+antdkfa@users.noreply.github.com>
2022-02-20 21:38:29 +0900

    One Day, One Solve

---
+++ 2022/Febuary/3-4Sum II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    unordered_map<int,int> um;
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int n = nums1.size();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                um[nums1[i] + nums2[j]]++;
+            }
+        }
+        
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                int now = nums3[i] + nums4[j];
+                now*=-1;
+                ans += um[now];
+            }
+        }
+        return ans;
+    }
+};
