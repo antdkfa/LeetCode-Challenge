e51a994b5798d02b1c9bc28beb3159cde40a3abc
antdkfa <44724253+antdkfa@users.noreply.github.com>
2023-02-06 16:15:01 +0900

    One Day, One Solve

---
+++ 2023/January/6-Shuffle the Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(n*2);
+        for(int i=0, j=0;i<n;i++,j+=2) {
+            ans[j] = nums[i];
+            ans[j+1] = nums[i+n];
+        }
+
+        return ans;
+    }
+};
