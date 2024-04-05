e3beda446a19a08472f2ad54b1e9c53cf7bb7843
antdkfa <44724253+antdkfa@users.noreply.github.com>
2024-04-05 18:36:04 +0900

    Update 1-Assign Cookies.cpp

--- 2024/January/1-Assign Cookies.cpp
+++ 2024/January/1-Assign Cookies.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int ans = 0, gIdx = 0, sIdx = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
         while(gIdx < g.size() && sIdx < s.size()) {
             if(g[gIdx] <= s[sIdx]) {
                 ans++, gIdx++, sIdx++;
